65b08b224d142c774bd81a8eb4c2eb86f8dc8258
Julia Barcelos <julaai@gmail.com>
2022-04-02 12:15:55 -0300

    Introduce function create and update error message and codes

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -1,38 +1,42 @@
 const ProductsService = require('../services/ProductsService');
-// const HTTPCodes = require('../schemas/HTTPCodes');
-// const ProductSchema = require('../schemas/ProductSchema');
-// const { errorMessage } = require('../schemas/ProductErrorsResponse');
-
-// const { validation, blank } = ProductSchema;
+const HTTPCodes = require('../schemas/HTTPCodes');
+const { message } = require('../schemas/ProductErrorsResponse');
 
 const getAll = async (req, res) => {
   try {
     const products = await ProductsService.getAll();
-    return res.status(200).json(products).end();
-
-    // if (blank(products)) {
-    //   return res.status(HTTPCodes.NOT_FOUND).json(errorMessage.noProductFound).end();
-    // }
-    // return res.status(HTTPCodes.OK).json(products).end();
+    return res.status(HTTPCodes.OK).json(products).end();
   } catch (error) {
     console.log(error);
-    // return res.status(HTTPCodes.LOCAL_ERROR).json(errorMessage.noGet).end();
-    return res.status(500).json({ message: 'Erro no Servidor' });
+    return res.status(HTTPCodes.LOCAL_ERROR).json({ message: message.noGet });
   }
 };
 
 const getById = async (req, res) => {
   try {
     const productId = await ProductsService.getById(req.params.id);
-  if (productId === undefined) return res.status(404).json({ message: 'Product not found' });
-  return res.status(200).json(productId).end();
+  if (productId === undefined) {
+    return res.status(HTTPCodes.NOT_FOUND).json({ message: message.productNotFound }).end();
+  }
+  return res.status(HTTPCodes.OK).json(productId).end();
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ message: 'Erro no Servidor' });
+    return res.status(HTTPCodes.LOCAL_ERROR).json({ message: message.noGet });
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const product = await ProductsService.create(req.body);
+    return res.status(HTTPCodes.CREATED).json(product).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(HTTPCodes.LOCAL_ERROR).json({ message: message.noGet });
   }
 };
 
 module.exports = {
   getAll,
   getById,
+  create,
 };
